583764084530bb7d9081b6cf40627cd7d1dc1c35
Palacios Gonzales Leonardo Alexander <leonardoalexander130@gmail.com>
2025-10-28 11:49:32 -0500

    Agregue la base de datos

---
+++ db/pymes.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE pymes;
+-- DROP DATABASE pymes;
+USE pymes;
+
+CREATE TABLE clientes (
+	id					INT PRIMARY KEY AUTO_INCREMENT,
+    nombres				VARCHAR(20) NOT NULL,
+    apellidos			VARCHAR(20) NOT NULL,
+    dni					CHAR(8) NOT NULL,
+	telefono			CHAR(9) NOT NULL,
+    CONSTRAINT uk_dni UNIQUE(dni)
+)ENGINE = INNODB;
+
+CREATE TABLE prestamos (
+	id					INT PRIMARY KEY AUTO_INCREMENT,
+    prestamo			DECIMAL(10, 2) NOT NULL,
+    caracteristicas		VARCHAR(100) NOT NULL,
+    letracambio			VARCHAR(200) NOT NULL,
+    fechainicio			DATETIME NOT NULL DEFAULT NOW(),
+    fechalimite			DATE NOT NULL,
+    transferencia    	VARCHAR(200) COMMENT "Transferencia o efectivo",
+    interes				DECIMAL(5, 2),
+    cliente				INT,
+    FOREIGN KEY (cliente) REFERENCES clientes(id) ON DELETE CASCADE
+)ENGINE = INNODB;
+
+CREATE TABLE pagos (
+	id					INT PRIMARY KEY AUTO_INCREMENT,
+    cuota				DECIMAL(10, 2) NOT NULL,
+    fechapago			DATETIME NOT NULL DEFAULT NOW(),
+    prestamo			INT NOT NULL,
+    FOREIGN KEY (prestamo) REFERENCES prestamos(id)
+)ENGINE = INNODB;
+
+INSERT INTO clientes (nombres, apellidos, dni, telefono) 
+			VALUES ("Fabián Alonzo", "Salas Vasquez", "74064042", "936200224"),
+				   ("Leonardo Alexander", "Palacios Gonzales", "75266220", "903386996");
+ 
+INSERT INTO prestamos (prestamo, caracteristicas, letracambio, fechalimite, transferencia, interes, cliente)
+			VALUES (1000, "caracteristicas", "letracambio.pdf", "2025-11-01", NULL, 30, 1);
+
+INSERT INTO pagos (cuota, prestamo) 
+			VALUES (255.00, 1);
+
+SELECT 
+    pag.id AS pago_id,
+    pag.cuota,
+    pre.prestamo AS prestamo,
+    cli.nombres
+FROM pagos pag
+INNER JOIN prestamos pre
+    ON pag.prestamo = pre.id
+INNER JOIN clientes cli
+    ON pre.cliente = cli.id;
+
+SELECT * FROM clientes;
+SELECT * FROM prestamos;
+SELECT * FROM pagos;
